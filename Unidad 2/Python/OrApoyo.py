8a50f29463eabb7d199f773186501e3893aa6c1a
Mary Macias <marylumacias1119@gmail.com>
2025-03-06 22:30:54 -0600

    Unidad 3

---
+++ Unidad 2/Python/OrApoyo.py
@@ -0,0 +1,11 @@
+# Programa para recibir apoyo or
+
+# Solicitar al usuario que ingrese su edad
+Edad = int(input("Ingresa tu edad: "))
+
+# Verificar cual es la edad del solicitante
+
+if Edad < 30 or < Edad 60 :
+    print("No eres apto.")
+else:
+    print("Puedes recibir apoyo.")
