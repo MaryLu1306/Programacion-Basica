e469634a55e3670d75667b2088b0bcea1cb41e72
Mary Macias <marylumacias1119@gmail.com>
2025-05-08 07:47:22 -0600

    Rendimiento del programa

--- Unidad 3/ExamenU3.py
+++ Unidad 3/ExamenU3.py
@@ -1,70 +1,81 @@
 from Archivos import guardar_diccionarios_en_csv, leer_diccionarios_de_csv
 import time
 
-archivo = "alumnos.csv"
 
-# Lista vacia de alumnos[]
-alumnos = leer_diccionarios_de_csv(archivo)
+def main():
+    tic = time.perf_counter()
 
-print("¡Te damos la bienvenida al Instituto Tecnológico de Pabellón de Arteaga!")
-print("Contamos con formacion en:")
-print("Ingenieria Mecatrónica")
-print("Ingenieria Industrial")
-print("Ingenieria en Gestión Empresarial")
-print("Ingenieria en Logistica")
-print("Maestría en Ingenieria Mecatrónica")
-print("Para empezar tienes que registrarte.")
+    archivo = "alumnos.csv"
 
-while True:
-    print("\n📋 LISTA DE ALUMNOS📋")
-    print("1. Registrar alumno")
-    print("2. Dar de baja a alumno")
-    print("3. Ver lista de alumnos")
-    print("4. Salir")
+    # Lista vacia de alumnos[]
+    alumnos = leer_diccionarios_de_csv(archivo)
 
-    opcion = input("Elige una opción: ")
+    print("¡Te damos la bienvenida al Instituto Tecnológico de Pabellón de Arteaga!")
+    print("Contamos con formacion en:")
+    print("Ingenieria Mecatrónica")
+    print("Ingenieria Industrial")
+    print("Ingenieria en Gestión Empresarial")
+    print("Ingenieria en Logistica")
+    print("Maestría en Ingenieria Mecatrónica")
+    print("Para empezar tienes que registrarte.")
 
-    if opcion == "1":
-        nombre = input("Ingrese su nombre: ")
-        carrera = input("Ingrese la carrera de su preferencia: ")
-        edad = input("Ingrese su edad: ")
-        calificacion = input("Ingrese su calificación: ")
+    while True:
+        print("\n📋 LISTA DE ALUMNOS📋")
+        print("1. Registrar alumno")
+        print("2. Dar de baja a alumno")
+        print("3. Ver lista de alumnos")
+        print("4. Salir")
 
-        alumnos.append({"nombre": nombre, "carrera": carrera, "edad": edad, "calificación": calificacion})
-        print(f"✅ Alumno {nombre} registrado correctamente.")
+        opcion = input("Elige una opción: ")
 
-    elif opcion == "2": 
-        if not alumnos:  
-            print("❌ No hay alumnos registrados, no puedes dar de baja a ninguno.")  
-        else:  
-            try:
-                num = int(input("Número de alumno a eliminar: ")) - 1
-                if 0 <= num < len(alumnos):
-                    eliminado = alumnos.pop(num)
-                    print(f"✅ Alumno '{eliminado['nombre']}' dado de baja.")
-                else:
-                    print("❌ Número no registrado.")
-            except ValueError:
-                print("❌ Ingresa un número válido.")
-        
-    elif opcion == "3":
-        if not alumnos:
-            print("📭 No hay alumnos registrados.")
+        if opcion == "1":
+            nombre = input("Ingrese su nombre: ")
+            carrera = input("Ingrese la carrera de su preferencia: ")
+            edad = input("Ingrese su edad: ")
+            calificacion = input("Ingrese su calificación: ")
+
+            alumnos.append({"nombre": nombre, "carrera": carrera, "edad": edad, "calificación": calificacion})
+            print(f"✅ Alumno {nombre} registrado correctamente.")
+
+        elif opcion == "2": 
+            if not alumnos:  
+                print("❌ No hay alumnos registrados, no puedes dar de baja a ninguno.")  
+            else:  
+                try:
+                    num = int(input("Número de alumno a eliminar: ")) - 1
+                    if 0 <= num < len(alumnos):
+                        eliminado = alumnos.pop(num)
+                        print(f"✅ Alumno '{eliminado['nombre']}' dado de baja.")
+                    else:
+                        print("❌ Número no registrado.")
+                except ValueError:
+                    print("❌ Ingresa un número válido.")
+
+        elif opcion == "3":
+            if not alumnos:
+                print("📭 No hay alumnos registrados.")
+            else:
+                print("\n📌 Lista de Alumnos:")
+                for i, alumno in enumerate(alumnos, 1):
+                    print(f"{i}. {alumno['nombre']} - Carrera: {alumno['carrera']} - Edad: {alumno['edad']} - Calificación: {alumno['calificación']}")
+
+        elif opcion == "4":
+            print("La actualización de listas se ha realizado correctamente. ¡Hasta luego!")
+            time.sleep(1)  
+            break
+
         else:
-            print("\n📌 Lista de Alumnos:")
-            for i, alumno in enumerate(alumnos, 1):
-                print(f"{i}. {alumno['nombre']} - Carrera: {alumno['carrera']} - Edad: {alumno['edad']} - Calificación: {alumno['calificación']}")
+            print("❌ Opción inválida. Intenta de nuevo.")
 
-    elif opcion == "4":
-        print("La actualización de listas se ha realizado correctamente. ¡Hasta luego!")
-        time.sleep(1)  
-        break
+    toc = time.perf_counter()
+    print(f"La actualizacion de listas se ha realizado en {toc - tic:0.4f} segundos")
 
-    else:
-        print("❌ Opción inválida. Intenta de nuevo.")
+    guardar_diccionarios_en_csv(archivo, alumnos)
 
-guardar_diccionarios_en_csv(archivo, alumnos)
+    datos_leidos = leer_diccionarios_de_csv(archivo)
+    print("Datos leídos del archivo CSV:")
+    print(datos_leidos)
 
-datos_leidos = leer_diccionarios_de_csv(archivo)
-print("Datos leídos del archivo CSV:")
-print(datos_leidos)
+if __name__ == "__main__":
+    main()
+
