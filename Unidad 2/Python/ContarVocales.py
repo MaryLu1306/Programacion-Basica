06269d2ae1639de3477e6142217caf7aeb06ead1
Mary Macias <marylumacias1119@gmail.com>
2025-05-25 14:18:37 -0600

    Guia de instalacion

---
+++ Unidad 2/Python/ContarVocales.py
@@ -0,0 +1,22 @@
+texto = input("Introduce un texto para analizar: ")
+
+contador_vocales = 0
+contador_consonantes = 0
+contador_numeros = 0
+contador_otros 
+
+vocales = ['a','A','á','E','e','é','i','I','í','O','o','ó','U','u']
+
+for caracter in texto:
+    caracter = caracter.lower()  
+
+    if caracter in vocales:
+        contador_vocales += 1
+    elif caracter.isalpha():
+        contador_consonantes += 1
+    elif caracter.isdigit():
+        contador_numeros += 1
+    
+print(f"El texto contiene {contador_vocales} vocales." )
+print(f"El texto contiene {contador_consonantes} consonantes")
+print(f"El texto contiene {contador_numeros} numeros")
